96c47f5d971b1853d8e2050372982683d12abddd
unknown <sanasultana4536@gmail.com>
2025-04-17 19:43:05 +0530

    uploading folder

---
+++ MyFirstProgram.java
@@ -0,0 +1,18 @@
+class MyFirstProgram{
+
+ public static void main(String[] args){
+
+  System.out.println("HI STUDENTS");
+
+
+}
+}
+
+//creating a file with .java extension
+  eg:program1.java
+//compile the code
+  cmd --> javac filename.java
+         ex: javac program1.java
+//run the code
+  -->java className.java
+     ex: java Program1.java
